ee607983fe26a304403ef67ce2aeecb4963eb2bf
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-07-10 21:04:05 +0200

    Update 1672_RichesCustomerWealth.c

--- 1672_RichesCustomerWealth.c
+++ 1672_RichesCustomerWealth.c
@@ -1,1 +1,12 @@
-
+int maximumWealth(int** accounts, int accountsSize, int* accountsColSize){
+    int i,j,sum; int max=0;
+    for(i=0;i<accountsSize;i++){
+        sum=0;
+        for(j=0;j<*accountsColSize;j++){
+            sum=sum+accounts[i][j];
+        }
+        if (sum>max) max=sum;
+    }
+    
+    return max;
+}
