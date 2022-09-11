318978749369a86dfd98a0c97a8ed2209472b795
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-09-11 10:23:51 +0200

    Time: 50 ms (12.00%), Space: 6.5 MB (56.00%) - LeetHub

---
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.c
@@ -0,0 +1,13 @@
+int valoreassoluto(int number){
+    if(number<0) return number*(-1);
+    return number;
+}
+
+int findClosestNumber(int* nums, int numsSize){
+    int i,min_distance=100001;
+    for(i=0;i<numsSize;i++){
+        if(valoreassoluto(nums[i])<=valoreassoluto(min_distance) && min_distance!= valoreassoluto(nums[i])) min_distance=nums[i];
+    }
+    return min_distance;
+}
+
