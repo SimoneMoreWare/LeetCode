6cfd6073406fde53919c7db97cf07678eaeb9674
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-09-27 10:15:14 +0200

    Time: 11 ms (85.06%), Space: 6.9 MB (75.53%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.c
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.c
@@ -1,17 +1,15 @@
-struct countword{
-    int letter[26];
-};
+
 
 int firstUniqChar(char * s){
-    struct countword countword;
+    int letter[26];
     int i;
     int lenght_string= (int)strlen(s);
 
-    for(i=0;i<26;i++) countword.letter[i]=0;
+    for(i=0;i<26;i++) letter[i]=0;
 
-    for(i=0;i< lenght_string;i++) countword.letter[s[i]-'a']++;
+    for(i=0;i< lenght_string;i++) letter[s[i]-'a']++;
 
-    for(i=0;i< lenght_string;i++) if(countword.letter[s[i]-'a']==1) return i;
+    for(i=0;i< lenght_string;i++) if(letter[s[i]-'a']==1) return i;
     
     return -1;
 }
