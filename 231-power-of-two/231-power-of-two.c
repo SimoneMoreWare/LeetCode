f7d685eeb17e00d03ca474821bd6cedb534c0915
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-09-30 13:32:19 +0200

    Time: 2 ms (65.94%), Space: 5.6 MB (54.43%) - LeetHub

--- 231-power-of-two/231-power-of-two.c
+++ 231-power-of-two/231-power-of-two.c
@@ -4,6 +4,5 @@ bool isPowerOfTwo(int n){
     if(n<=0) return false;
     if(n==1) return true;
     if(n%2!=0) return false;
-    n=n/2;
-    return isPowerOfTwo(n);
+    return isPowerOfTwo(n/2);
 }
