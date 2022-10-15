fb95ea5b98ff17de2f679db917390eb278cd3ef0
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-10-15 08:40:51 +0200

    Time: 28 ms (76.90%), Space: 8 MB (16.20%) - LeetHub

--- 0169-majority-element/0169-majority-element.c
+++ 0169-majority-element/0169-majority-element.c
@@ -1,30 +1,35 @@
-int majorityR(int *a,int l,int r,int numberofmajor){
+int countInRange(int a[], int num, int l, int r) {
+        int count = 0;
+        for (int i = l; i <= r; i++) {
+            if (a[i] == num) {
+                count++;
+            }
+        }
+        return count;
+}
 
-    int countSx=0,countDx=0;
-    int tmpSx=a[l],tmpDx=a[r];
-    int i=l,j=r;
+int majorityR(int *a,int l,int r){
+   
+    int m=(r-l)/2+l;
+    int left,right;
+    int leftCount,rightCount;
     
-    if(numberofmajor==0) return a[0];
-    if(l>=r) return -1;
-
-    while(i<=j){
-        if (a[i]==tmpDx) countDx++;
-        else if (a[i]==tmpSx) countSx++;
-
-        if (a[j]==tmpDx) countDx++;
-        else if (a[j]==tmpSx) countSx++;
-
-        i++;
-        j--;
-    }
-
-    if(countDx>numberofmajor) return tmpDx;
-    if(countSx>numberofmajor) return tmpSx;
-
-    return majorityR(a,++l,--r,numberofmajor);
+    if(l==r) return a[l];
+    
+    left=majorityR(a,l,m);
+    right=majorityR(a,m+1,r);
+    
+    if(left==right) return left;
+    
+    leftCount=countInRange(a,left,l,r);
+    rightCount=countInRange(a,right,l,r);
+    
+    return leftCount > rightCount ? left : right;
+    
+    
 }
 
 int majorityElement(int* nums, int numsSize){
-    return majorityR(nums,0,numsSize-1,numsSize/2);
+    return majorityR(nums,0,numsSize-1);
 }
 
