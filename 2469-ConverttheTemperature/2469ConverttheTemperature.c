5264ddc8e8c92ccc16768cf99f302f45ddf91147
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2023-06-30 11:48:32 +0200

    Create 2469ConverttheTemperature.c

---
+++ 2469-ConverttheTemperature/2469ConverttheTemperature.c
@@ -0,0 +1,15 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+double* convertTemperature(double celsius, int* returnSize){
+
+    double *ans = (double *) malloc(2*sizeof(double));
+    
+    ans[0] = celsius + 273.15;
+    ans[1] = (celsius * 1.80) + 32;
+
+    (*returnSize) = 2;
+
+    return ans;
+
+}
