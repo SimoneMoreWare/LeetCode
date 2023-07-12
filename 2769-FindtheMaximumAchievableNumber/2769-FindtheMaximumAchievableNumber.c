9b90d8f338e6d6427f7374f5c4ca50b142e7ae44
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2023-07-12 13:30:07 +0200

    Create 2769-FindtheMaximumAchievableNumber.c

---
+++ 2769-FindtheMaximumAchievableNumber/2769-FindtheMaximumAchievableNumber.c
@@ -0,0 +1,8 @@
+int theMaximumAchievableX(int num, int t){
+    return num+t+t;
+}
+
+/*
+2769-FindtheMaximumAchievableNumber
+https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+*/
