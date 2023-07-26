9316f5ada2bb77b586b0d6c5f9829311bfd76aa7
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2023-07-26 21:49:24 +0200

    Create 2651-CalculateDelayedArrivalTime.c

---
+++ 2651-CalculateDelayedArrivalTime/2651-CalculateDelayedArrivalTime.c
@@ -0,0 +1,8 @@
+int findDelayedArrivalTime(int arrivalTime, int delayedTime){
+    return (arrivalTime + delayedTime)%24;
+}
+
+/*
+2651-CalculateDelayedArrivalTime
+https://leetcode.com/problems/calculate-delayed-arrival-time/description/
+*/
